2db50c5555a130c8ef3c69b3ad14573902626fc9
Abhishekjain0112 <abhishekjain0112@gmail.com>
2019-12-28 14:27:34 +0530

    first commit

---
+++ WorkShopTest/src/workshoptest/WorkShopTest.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package workshoptest;
+
+import javax.swing.JFrame;
+
+public class WorkShopTest {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        JFrame f=new NewJFrame();
+        f.setVisible(true);
+    }
+    
+}
